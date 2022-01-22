0ace084f13031f8506ebe5f0b1c2c462ab8fad21
albertmakan <makanalbert@gmail.com>
2022-01-22 13:30:38 +0100

    created dataset

--- pythonProject/create_dataset.py
+++ pythonProject/create_dataset.py
@@ -1,10 +1,16 @@
 import cv2
 import mediapipe as mp
+import os
 
 from utils import extract_hand
 
 
 def main():
+    label = input("Label: ")
+    path = "../dataset/"+label
+    if not os.path.exists(path):
+        os.makedirs(path)
+
     capture = cv2.VideoCapture(0)
     capture.set(3, 1280)
     capture.set(4, 720)
@@ -14,7 +20,7 @@ def main():
             model_complexity=0,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5) as hands:
-        while capture.isOpened():
+        while capture.isOpened() and n < 1000:
             success, image = capture.read()
             if not success:
                 continue
@@ -28,7 +34,7 @@ def main():
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
                     hand_img = extract_hand(hand_landmarks, image)
-                    # TODO save hand_img
+                    cv2.imwrite(f"{path}/{n}.png", hand_img)
                     n += 1
 
             cv2.imshow('Creating dataset', cv2.flip(image, 1))
